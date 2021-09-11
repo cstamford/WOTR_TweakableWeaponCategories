506151e1cde969eaf1f13792be819a004a97b002
cstamford <cstamford@gmail.com>
2021-09-11 15:02:37 +0100

    Weapon proficiency categories are now determined by the subcategory of the weapon category rather than by features.

--- TweakableWeaponCategories/Main.cs
+++ TweakableWeaponCategories/Main.cs
@@ -1,16 +1,17 @@
-
-using System;
-using System.Linq;
-using UnityEngine;
-using UnityModManagerNet;
+using HarmonyLib;
 using Kingmaker.Blueprints.Items.Weapons;
 using Kingmaker.Designers.Mechanics.Buffs;
 using Kingmaker.Enums;
+using Kingmaker.Items;
+using Kingmaker.UnitLogic;
 using Kingmaker.Utility;
+using System;
 using System.Collections.Generic;
-using HarmonyLib;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using UnityEngine;
+using UnityModManagerNet;
 
 namespace TweakableWeaponCategories
 {
@@ -138,12 +139,57 @@ namespace TweakableWeaponCategories
         }
     }
 
+    // This patch redirects a call to UnitProficiency.Contains to our own stub.
+    // Each proficiency feature contains a list of weapons that are allowed. This is different from weapon subcategories.
+    // We will make them the same, e.g. if the subcategory includes simple, then simple weapon proficiency allows it to be equipped.
+    [HarmonyPatch(typeof(ItemEntityWeapon), nameof(ItemEntityWeapon.CanBeEquippedInternal))]
+    public static class ItemEntityWeapon_CanBeEquippedInternal
+    {
+        private static MethodInfo Method__UnityProficiency_Contains = AccessTools.Method(typeof(UnitProficiency), nameof(UnitProficiency.Contains), new Type[] { typeof(WeaponCategory) });
+        private static MethodInfo Method__CanBeEquipped = AccessTools.Method(typeof(ItemEntityWeapon_CanBeEquippedInternal), nameof(ItemEntityWeapon_CanBeEquippedInternal.CanBeEquipped));
+
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> il = instructions.ToList();
+
+            for (int i = 0; i < il.Count; ++i)
+            {
+                if (il[i].Calls(Method__UnityProficiency_Contains))
+                {
+                    il[i].opcode = OpCodes.Ldarg_1; // load owner
+                    il.Insert(i + 1, new CodeInstruction(OpCodes.Call, Method__CanBeEquipped));
+                }
+            }
+
+            return il.AsEnumerable();
+        }
+
+        public static bool HasFeature(this UnitDescriptor _this, string guid)
+        {
+            return _this.Unit.Facts.Get<Feature>((i) => i.Blueprint.AssetGuid == guid) != null;
+        }
+
+        private static bool CanBeEquipped(UnitProficiency _, WeaponCategory category, UnitDescriptor owner)
+        {
+            if (TweakableWeaponCategories.Enabled)
+            {
+                bool allowed_simple = TweakableWeaponCategories.HasSubCategory(category, WeaponSubCategory.Simple) && owner.HasFeature("e70ecf1ed95ca2f40b754f1adb22bbdd");
+                bool allowed_martial = TweakableWeaponCategories.HasSubCategory(category, WeaponSubCategory.Martial) && owner.HasFeature("203992ef5b35c864390b4e4a1e200629");
+                bool allowed_monk = TweakableWeaponCategories.HasSubCategory(category, WeaponSubCategory.Monk) && owner.HasFeature("c7d6f5244c617734a8a76b6785a752b4");
+                return allowed_simple || allowed_martial || allowed_monk;
+            }
+
+            return owner.Proficiencies.Contains(category);
+        }
+    }
+
     // This is an patch to change the BlueprintItemWeapon::IsMonk call - this is because the code never actually checks
     // if a weapon has the Monk subcategory, it only checks whether the blueprint is flagged as a monk item.
     [HarmonyPatch(typeof(MonkNoArmorAndMonkWeaponFeatureUnlock), nameof(MonkNoArmorAndMonkWeaponFeatureUnlock.CheckEligibility))]
     public static class Minimap_SetMapMode
     {
-        private static MethodInfo Method__get_IsMonk = AccessTools.Method(typeof(BlueprintItemWeapon), "get_IsMonk");
+        private static MethodInfo Method__IsMonk = AccessTools.PropertyGetter(typeof(BlueprintItemWeapon), nameof(BlueprintItemWeapon.IsMonk));
         private static MethodInfo Method__IsMonkWeapon = AccessTools.Method(typeof(Minimap_SetMapMode), nameof(Minimap_SetMapMode.IsMonkWeapon));
 
         [HarmonyTranspiler]
@@ -153,7 +199,7 @@ namespace TweakableWeaponCategories
 
             foreach (CodeInstruction inst in il)
             {
-                if (inst.Calls(Method__get_IsMonk))
+                if (inst.Calls(Method__IsMonk))
                 {
                     inst.opcode = OpCodes.Call;
                     inst.operand = Method__IsMonkWeapon;
@@ -165,14 +211,7 @@ namespace TweakableWeaponCategories
 
         private static bool IsMonkWeapon(BlueprintItemWeapon instance)
         {      
-            bool is_monk_item = instance.IsMonk;
-
-            if (TweakableWeaponCategories.Enabled)
-            {
-                is_monk_item = is_monk_item || TweakableWeaponCategories.HasSubCategory(instance.Category, WeaponSubCategory.Monk);
-            }
-
-            return is_monk_item;
+            return TweakableWeaponCategories.Enabled ? TweakableWeaponCategories.HasSubCategory(instance.Category, WeaponSubCategory.Monk) : instance.IsMonk;
         }
     }
 }
